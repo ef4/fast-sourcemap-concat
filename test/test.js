54c2a13a04fbc8ddda74dc3ee66a0d9d3e4a690e
Edward Faulkner <ef@alum.mit.edu>
2014-12-15 18:29:37 -0500

    adding experimental cache

--- test/test.js
+++ test/test.js
@@ -80,6 +80,56 @@ describe('fast sourcemap concat', function() {
     });
   });
 
+  it("should populate cache", function() {
+    var cache = {};
+    var s = new SourceMap({outputFile: 'tmp/external-content.js', baseDir: path.join(__dirname, 'fixtures'), cache: cache});
+    s.addFile('other/third.js');
+    s.addSpace("/* My First Separator */");
+    s.addFile('external-content/all-inner.js');
+    s.addSpace("/* My Second */");
+    s.addFile('other/fourth.js');
+    return s.end().then(function(){
+      expectFile('external-content.js').in('tmp');
+      expectFile('external-content.map').in('tmp');
+      assert.deepEqual(cache, {
+        "7743064ff83a0c5a7558e247eb90b37b": { encoder : "AAEAA", lines : 3 },
+        "2a257e37006faed088631037626f5117": { encoder: "AEAAA", lines: 11 },
+        "22ae6265717075a5ee113b8c4274b5a2": { encoder: "ACGAA", lines: 6 }
+      });
+    });
+  });
+
+  it("should use cache", function() {
+    var cache = {};
+
+    function once(finalFile){
+      var s = new SourceMap({cache: cache, outputFile: 'tmp/intermediate.js'});
+      s.addFile('fixtures/inner/first.js');
+      var filler = "'x';";
+      s.addSpace(filler);
+      s.addFile('fixtures/inner/second.js');
+
+      return s.end().then(function(){
+        s = new SourceMap({cache: cache, outputFile: 'tmp/intermediate2.js'});
+        s.addFile('fixtures/other/fourth.js');
+        return s.end();
+      }).then(function(){
+        s = new SourceMap({cache: cache, outputFile: 'tmp/' + finalFile});
+        s.addFile('tmp/intermediate.js');
+        s.addFile('fixtures/other/third.js');
+        s.addFile('tmp/intermediate2.js');
+        return s.end();
+      });
+    }
+
+    return once('firstPass.js').then(function(){
+      return once('final.js');
+    }).then(function(){
+      expectFile('final.js').in('tmp');
+      expectFile('final.map').in('tmp');
+    });
+  });
+
 });
 
 function expectFile(filename) {
